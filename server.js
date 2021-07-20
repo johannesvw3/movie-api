00a348dccbb4fe074ea97574b009dcac207eaba4
Johannes van Woerkom <johannesvw3@gmail.com>
2021-07-20 12:02:51 -0400

    fixed "url.parse" issue, Server is now running

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http'),
 
 http.createServer((request, response) => {
   let addr = request.url,
-    q = url.use(addr, true),
+    q = url.parse(addr, true),
     filePath = '';
 
   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
