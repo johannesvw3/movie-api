28200a323f74d0b6dca1fdd3600f463e880ea647
Johannes van Woerkom <johannesvw3@gmail.com>
2021-08-28 11:21:59 -0400

    Added Env. Variable through Heroku / Imported DB to Heroku

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const { check, validationResult } = require('express-validator');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+/*mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true }); */
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Movies = Models.Movie;
 const Users = Models.User;
