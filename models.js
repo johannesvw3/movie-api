d9e33bebb6bce00b7757261e6d14e9702e4803c2
Johannes van Woerkom <johannesvw3@gmail.com>
2021-09-02 19:20:51 -0400

    Update models.js

--- models.js
+++ models.js
@@ -6,7 +6,7 @@ let movieSchema = mongoose.Schema({
     Description: {type: String, required: true},
     Genre: { type: mongoose.Schema.Types.ObjectId, ref: 'Genre' },
     Director: {type: mongoose.Schema.Types.ObjectId, ref: 'Director' },
-    Actors: [string],
+    Actors: [String],
     ImagePath: String,
     Featured: Boolean
 });
